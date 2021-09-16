937e85917d8901c58e734561b4c5fb7529c9e8cf
Coleton Watt <electricninja102@gmail.com>
2021-09-16 07:53:30 -0600

    Initial Commit

---
+++ Unit 4 No Brainer: Battle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Unit 4 No Brainer: Battle
+//
+//  Created by Coleton Watt on 9/16/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
